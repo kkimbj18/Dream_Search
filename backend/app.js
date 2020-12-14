4c9bb5a856b50fa704d8e51a94345882c9d96424
root <root@DESKTOP-QJBICG3.localdomain>
2020-12-15 02:29:01 +0900

    edit app for cloud service

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, '../frontend/build/index.html')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
